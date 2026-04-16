a4ed65661861fa5897c4363c76dcdcbfa422a5ee
hossainsiam133 <152617928+hossainsiam133@users.noreply.github.com>
2026-04-16 17:41:33 +0600

    upgraded net and docker to 10

--- Bangla-Bazar.Server/obj/Debug/net10.0/Bangla-Bazar.Server.AssemblyInfo.cs
+++ Bangla-Bazar.Server/obj/Debug/net10.0/Bangla-Bazar.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bangla-Bazar.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d4ace1aa6ea7cfa5a9e7ac236ad8df873131e57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+310b8da2fc98be658796685dbea33a9f0196f540")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bangla-Bazar.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bangla-Bazar.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
